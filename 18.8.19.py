b7af2d366fa067caa527be677bd5d6adb43c65f9
AGritsenko <74305358+zxacus@users.noreply.github.com>
2024-03-04 23:18:59 +0300

    HW_18.8.19.py

--- 18.8.19.py
+++ 18.8.19.py
@@ -1,9 +1,10 @@
 tickets = int(input('При заказе от 4 билетов скидка 10%\nВведите количество билетов: '))
+while tickets < 0:
+    tickets = int(input('Количество билетов не может быть отрицательным\nВведите количество билетов: '))
 if not tickets:
+    print(tickets)
     print('Ждём вас снова')
     quit()
-if tickets < 0:
-    tickets = int(input('Количество билетов не может быть отрицательным\nВведите количество билетов: '))
 
 clients_age = [int(input(f'Введите возраст {i + 1} посетителя: ')) for i in range(tickets)]
 while any(val < 0 or val > 144 for val in clients_age):
